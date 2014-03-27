12b72af94a71ccf76f33ebbf0023665110102fdd
czxttkl <czxttkl@gmail.com>
2014-03-27 14:16:36 -0400

    remove graph from main screen

--- src/io/github/czxttkl/game/graph/GraphActivity.java
+++ src/io/github/czxttkl/game/graph/GraphActivity.java
@@ -1,14 +1,10 @@
-package io.github.czxttkl.game.mainscreen;
+package io.github.czxttkl.game.graph;
 
 import io.github.czxttkl.game.Challenge;
 import io.github.czxttkl.game.ChallengeActivity;
 import io.github.czxttkl.game.ChallengeLab;
-import io.github.czxttkl.game.graph.GraphActivity;
-import io.github.czxttkl.game.join.JoinActivity;
+import io.github.czxttkl.game.mainscreen.MainScreenActivity;
 import io.github.czxttkl.game.progress.ProgressActivity;
-
-import com.actionbarsherlock.sample.shakespeare.R;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ComponentName;
@@ -17,14 +13,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainScreenActivity extends Activity {
+import com.actionbarsherlock.sample.shakespeare.R;
+
+public class GraphActivity extends Activity{
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// setTitle(R.string.activity_titles);
-		setContentView(R.layout.mainscreen);
+		setContentView(R.layout.graph);
 	}
-
+	
 	public void onClickNew(View v) {
 		final CharSequence[] items = { "New Challenge", "Upload My Progress" };
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -33,41 +32,18 @@ public class MainScreenActivity extends Activity {
 			public void onClick(DialogInterface dialog, int item) {
 				if (item == 0) {
 					Challenge crime = new Challenge();
-					ChallengeLab.get(MainScreenActivity.this).addCrime(crime);
+					ChallengeLab.get(GraphActivity.this).addCrime(crime);
 					Intent i = new Intent();
-					i.setComponent(new ComponentName(MainScreenActivity.this, ChallengeActivity.class));
+					i.setComponent(new ComponentName(GraphActivity.this, ChallengeActivity.class));
 					startActivity(i);
 				} else {
 					Intent i = new Intent();
-					i.setComponent(new ComponentName(MainScreenActivity.this, ProgressActivity.class));
+					i.setComponent(new ComponentName(GraphActivity.this, ProgressActivity.class));
 					startActivity(i);
 				}
 			}
 		});
 		AlertDialog alert = builder.create();
 		alert.show();
-
-	}
-
-	public void onClickJoin(View v) {
-		Intent i = new Intent();
-		i.setComponent(new ComponentName(this, JoinActivity.class));
-		startActivity(i);
-	}
-
-	public void onClickQuit(View v) {
-		this.finish();
-	}
-
-	public void onClickProgress(View v) {
-		Intent i = new Intent();
-		i.setComponent(new ComponentName(this, ProgressActivity.class));
-		startActivity(i);
-	}
-	
-	public void onClickAvatar(View v) {
-		Intent i = new Intent();
-		i.setComponent(new ComponentName(this, GraphActivity.class));
-		startActivity(i);
 	}
 }
